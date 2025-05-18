ae080e4c7d842a47bcd7faaf3a958fe56bf90c75
Priyanka yadav <priyanyadav52004@gmail.com>
2025-05-18 17:36:39 +0530

    logout issue fix

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,6 +3,7 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 import { User } from "../models/user.model.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
+import mongoose from "mongoose";
 import jwt from "jsonwebtoken";
 
 const generateAccessAndRefreshTokens = async (userId) => {
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -70,7 +70,7 @@ userSchema.methods.generateAccessToken = function () {
       username: this.username,
     },
     process.env.ACCESS_TOKEN_SECRET,
-    { expiresIn: process.env.ACCESS_TOKEN_EXPIRY } // also fix this
+    { expiresIn: process.env.ACCESS_TOKEN_EXPIRY } 
   );
 };
 
